04a31448218a759531a5ebd5eb600668a958cb4b
starnie <63254343+starnie@users.noreply.github.com>
2020-06-09 22:25:03 +0200

    Updates

---
+++ precache-manifest.7c3f8c8c53656515512c419ef70b26b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f86e7f9808883ac47d235607911cc3",
+    "url": "/wccapp/index.html"
+  },
+  {
+    "revision": "ffcb49e7ce3a9a51d252",
+    "url": "/wccapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "467a1a9c8ba844e2e5cf",
+    "url": "/wccapp/static/css/main.5f31887d.chunk.css"
+  },
+  {
+    "revision": "ffcb49e7ce3a9a51d252",
+    "url": "/wccapp/static/js/2.786920e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wccapp/static/js/2.786920e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467a1a9c8ba844e2e5cf",
+    "url": "/wccapp/static/js/main.e31b5d34.chunk.js"
+  },
+  {
+    "revision": "5e477009c7de4e45a56e",
+    "url": "/wccapp/static/js/runtime-main.ad652bda.js"
+  }
+]);
